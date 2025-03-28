a72f970181290933b42f04806f67852a143a8b3e
Draconico-blu <167083051+Draconico-blu@users.noreply.github.com>
2025-03-27 21:12:14 -0300

    criacao projeto e aulas

---
+++ Java/Dio/src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        //variaveis
+        String meuNome = "Lucas";
+        int anoFabriecacao = 2025;
+        boolean teste = false;
+
+        //atualizar variavel
+        anoFabriecacao = 2028;
+
+    }
+}
