9194ae41d21d4983307d605bc9963f08833c46ef
Draconico-blu <167083051+Draconico-blu@users.noreply.github.com>
2025-05-03 15:35:40 -0300

    iniciando a questao 4

--- AtvddsUniversity/Estruturas de Dados I/Exercício4/Q4.c
+++ AtvddsUniversity/Estruturas de Dados I/Exercício4/Q4.c
@@ -1,3 +1,7 @@
+/*
+Usando a estrutura Vendas criada aqui nos slides, e a função TotalVendas, crie o programa que lê três vendas do teclado e depois mostra o 
+total de vendas usando apenas a função TotalVendas; Que slader omi :(
+*/
 
 #include <stdio.h>
 
