4c79f9492dd9bd912375546c0ba70f18da2b6556
Draconico-blu <167083051+Draconico-blu@users.noreply.github.com>
2025-05-02 21:12:05 -0300

    Questao 2

--- AtvddsUniversity/Estruturas de Dados I/Exercício4/Q2.c
+++ AtvddsUniversity/Estruturas de Dados I/Exercício4/Q2.c
@@ -6,19 +6,43 @@ o	autor (outra cadeia de caracteres com tamanho máximo de 50);
 o	ano_publicacao (um inteiro).
 Utilize typedef para criar um “alias” TipoLivro para essa estrutura. Declare e leia três variáveis do tipo TipoLivro com informações de livros diferentes. Em seguida, imprima os detalhes de cada livro (título, autor e ano de publicação);
 
-
 */
 
 #include <stdio.h>
 
+typedef struct {
+    char titulo[50];
+    char autor[50];
+    int ano_publicacao;
+} TipoLivro;
+
+
 int main() {
-    //variaveis
+    // chamar struct
+    TipoLivro livros[3];
 
-    //entrada
+    // entradas
+    for(int i = 0; i < 3; i++) {
+        printf("Digite o titulo do livro %d: ", i + 1);
+        fgets(livros[i].titulo, 50, stdin);
+        
+        printf("Digite o autor do livro %d: ", i + 1);
+        fgets(livros[i].autor, 50, stdin);
 
-    //logica
+        printf("Digite o ano de publicacao do livro %d: ", i + 1);
+        scanf("%d", &livros[i].ano_publicacao, i + 1);
 
+        fflush(stdin);// limpar buffer
+        system("cls");// limpar cmd
+    }
+    
     //saida
+    for(int i = 0; i < 3; i++) {
+        printf("\nLivro %d:\n", i + 1);
+        printf("Autor: %s", livros[i].autor);
+        printf("Titulo: %s", livros[i].titulo);
+        printf("Ano de publicacao: %d\n", livros[i].ano_publicacao);
+    }
 
     return 0;
 }
---
+++ AtvddsUniversity/Estruturas de Dados I/Exercício4/Q3.c
@@ -0,0 +1,19 @@
+/*
+
+03)	Crie uma estrutura chamada Tempo que armazene horas (inteiro), minutos (inteiro) e segundos (inteiro). Use typedef para renomeá-la para Horario. Escreva um programa que declare duas variáveis do tipo Horario, leia os valores do teclado e calcule a diferença em segundos entre os dois horários. Se o segundo horário for anterior ao primeiro, inverta-os. Imprima os dois horários no formato "HH:MM:SS" e a diferença em segundos
+
+*/
+
+#include <stdio.h>
+
+int main() {
+    //variaveis
+
+    //entrada
+
+    //logica
+
+    //saida
+
+    return 0;
+}
