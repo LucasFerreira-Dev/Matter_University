ddb2bfc51dc6236cdb2c266830c5bc29b3de49f4
Draconico-blu <167083051+Draconico-blu@users.noreply.github.com>
2025-05-06 17:29:27 -0300

    Questao 5

--- AtvddsUniversity/Estruturas de Dados I/Exercício4/Q5.c
+++ AtvddsUniversity/Estruturas de Dados I/Exercício4/Q5.c
@@ -1,22 +1,54 @@
+/*
+
+05)	Utilizando a estrutura ao lado, faça uma função para ler o número e as 3 notas de 4 alunos, e depois mostrar as notas e a média no principal;
+
+*/
 
 #include <stdio.h>
 
 typedef struct {
-    int horas;
-} Tempo;
+    int numero;
+    float notas[3];
+} Aluno;
 
+// Função notas
+void lerNotasAlunos(Aluno alunos[], int tamanho) {
+    for (int i = 0; i < tamanho; i++) {
+        printf("Digite o numero do aluno %d: ", i + 1);
+        scanf("%d", &alunos[i].numero);
+
+        for (int j = 0; j < 3; j++) {
+            printf("Digite a nota %d do aluno %d: ", j + 1, alunos[i].numero);
+            scanf("%f", &alunos[i].notas[j]);
+        }
+    }
+}
+
+// Função para media
+float calcularMedia(float notas[], int tamanho) {
+    float soma = 0;
+    for (int i = 0; i < tamanho; i++) {
+        soma += notas[i];
+    }
+    return soma / tamanho;
+}
 
 int main() {
-    // chamar struct
-    Tempo tempo;
+    // chama struct e faz array
+    Aluno alunos[4];
 
-    //variaveis
+    // Chamada da função 
+    lerNotasAlunos(alunos, 4);
 
-    //entrada
-
-    //logica
-
-    //saida
+    // Exibição das notas e médias
+    for (int i = 0; i < 4; i++) {
+        printf("\nAluno %d:\n", alunos[i].numero);
+        for (int j = 0; j < 3; j++) {
+            printf("Nota %d: %.2f\n", j + 1, alunos[i].notas[j]);
+        }
+        float media = calcularMedia(alunos[i].notas, 3);
+        printf("Media: %.2f\n", media);
+    }
 
     return 0;
 }
--- AtvddsUniversity/Estruturas de Dados I/Exercício4/Q6.c
+++ AtvddsUniversity/Estruturas de Dados I/Exercício4/Q6.c
@@ -1,4 +1,8 @@
+/*
 
+06)	Defina uma estrutura chamada PerfilSaude que contenha os seguintes campos: Nome Completo: string com 80 caracteres; Sexo: um caractere; Data de nascimento: dia, mês e ano separados (usando outra estrutura); Altura: um número real; Peso: um número real; Idade atual: um inteiro; o índice de Massa Corporal: um número real; e a frequência cardíaca máxima: um número real; Faça um programa que tenha uma função que receba os dados Nome, Sexo, Data de Nascimento, Altura e Peso; e os utilize para definir os membros de uma variável PerfilSaude, usando alocação dinâmica de memória; O programa deve incluir outra função que calcule a idade atual do usuário em anos, a frequência cardíaca máxima e o índice de Massa Corporal, retornando a variável estrutura que foi recebida já com os novos dados incluídos nela; Em seguida deverão ser exibidos todos os dados lidos, além da idade (em anos), seu IMC, e sua frequência cardíaca máxima, através de ponteiros (não use a variável, use um ponteiro pra variável e mostre os valores através dele;
+
+*/
 #include <stdio.h>
 
 typedef struct {
