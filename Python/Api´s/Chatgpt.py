65cb6797ed6c31296f3d13042d6d985d747ebb9f
LucasFerreira-Dev <lucasgabrieldasilvaferreira76@gmail.com>
2025-05-31 19:49:27 -0300

    apis

---
+++ Python/Api´s/Chatgpt.py
@@ -0,0 +1,4 @@
+#links utes
+linkDocumentação = "https://platform.openai.com/docs/guides/text?api-mode=responses"
+aulaYoutube = "https://youtu.be/ZwfZlqTzsuA?si=rl_YtHxX4KGoaq02"
+
---
+++ Python/Api´s/ias.py
@@ -0,0 +1,5 @@
+gratis = "https://huggingface.co/docs/hub/models-the-hub"
+ollama = "https://ollama.com/"
+
+print("IA: " + gratis)
+print("IA: " + ollama)
