540da4785a4248af3f612ad9abfc1954c3e2cda6
Lucas <lucasgabrieldasilvaferreira76@gmail.com>
2025-09-16 10:40:38 -0300

    cursos

---
+++ Projetos/Projeto_Ferias/teste.c
@@ -0,0 +1,8 @@
+#include <stdlib.h>
+
+int main() {
+    // Vai abrir no programa associado ao .xlsx (normalmente o Excel)
+    //system("start C:\\Users\\lucas\\Desktop/Cronograma.xlsx");
+    system("start C:\\Users\\lucas\\Documents\\PROJETO_FERIAS/atividades.h");
+    return 0;
+}
