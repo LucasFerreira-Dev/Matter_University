3c4e9a4768b292a940f13bffff8541f3178942f6
Lucas <lucasgabrieldasilvaferreira76@gmail.com>
2025-09-18 19:50:14 -0300

    finalizando a atvdd de criar api medicos

--- AtvddsUniversity/Optativa_Typescript/src/index.ts
+++ AtvddsUniversity/Optativa_Typescript/src/index.ts
@@ -1,81 +1,113 @@
-import express from "express"; //framework para criar api
-import { Request, Response} from "express";
+import Express from "express";
+import { Request, Response } from "express";
 
-const app = express();
+const app = Express();
 const port: number = 3000;
 
-app.use(express.json());
+app.use(Express.json());
 
-const users = [
-    {id: 1, nome: "Ana", idade: 30},
-    {id: 2, nome: "João", idade: 20}
+const medicos = [
+    {id: 1, crm: "123456", nome: "Lucas", especialidade: "Cardiologista"}
 ];
-
-// ENDPOINTS
-app.get("/users", (req, res) => {
-    res.json(users);
-});
-
-//findOne
-app.get("/users/:id", (req, res) =>{
-    const idProcurado = Number(req.params.id);
-    console.log(idProcurado);
-    const usuarioEncontrado = users.find(user => user.id === idProcurado);
-
-    
-    console.log(usuarioEncontrado);
-
-    if (usuarioEncontrado) {
-        res.json(usuarioEncontrado);
-    } else {
-        res.status(404).json({mensagem: "Usuário não encontrado"});
-    }
-});
-
-app.post("/users", (req: Request, res: Response) => {
-    const { nome, idade } = req.body;
-
-    if (!nome || !idade) {
-        return res.status(400).json({ mensagem: "Nome e idade são obrigatórios" });
-    }
-
-    // Gera o próximo ID baseado no último do array
-    const newId = users.length > 0 ? users[users.length - 1].id + 1 : 1;
-
-    const newUser = { id: newId, nome, idade };
-    users.push(newUser);
-
-    res.status(201).json(newUser);
-});
-
-
-app.put("/users/:id", (req, res) => {
-    const id = Number(req.params.id);
-    const  { nome, idade } = req.body;
-
-    const userIndex = users.findIndex( u => u.id === id);
-
-    if(userIndex >= 0) {
-        users[userIndex] = { id, nome, idade};
-        res.json(users[userIndex]);
-    } else {
-        res.status(404).json({ mensagem: "Usuário não encontrado"});
-    }
-});
-
-app.delete("/users/:id", (req, res) => {
-    const id = Number(req.params.id);
-    const userIndex = users.findIndex(u => u.id === id);
-
-    if(userIndex >= 0){
-        const removeUser = users.splice(userIndex, 1)[0];
-        res.json({ mensagem: "Usuário removido", usuario: removeUser });
-    } else {
-        res.status(404).json({ mensagem: "Usuário não encontrado"});
-    }
-});
 
 app.listen(port, () => {
     console.log(`A api subiu na porta ${port}`);
 });
 
+app.get("/medicos", (req, res) => {
+    res.json(medicos);
+});
+
+app.get("/medicos/procurar/id/:id", (req, res) => {
+    const idProcurado = Number (req.params.id);
+    const medicoEncontrado = medicos.find(medico => medico.id == idProcurado);
+
+    if(medicoEncontrado){
+        res.json(medicoEncontrado);
+    } else {
+        res.status(404).json({mensagem: "Médico não encontrado"});
+    }
+});
+
+app.get("/medicos/procurar/crm/:crm", (req, res) => {
+    const crmProcurado = String (req.params.crm);
+    const medicoEncontrado = medicos.find(medico => medico.crm == crmProcurado);
+
+    if(medicoEncontrado){
+        res.json(medicoEncontrado);
+    } else {
+        res.status(404).json({mensagem: "Médico não encontrado"});
+    }
+});
+
+app.post("/medicos/criar", (req: Request, res: Response) => {
+    const { crm, nome, especialidade } = req.body;
+    if(!crm || !nome || !especialidade){
+        return res.status(400).json({mensagem: "CRM, nome, especialidade são obrigatorios"});
+    }
+
+    const newId = medicos.length > 0 ? medicos[medicos.length - 1].id + 1 : 1;
+    const newMedico = { id: newId, crm, nome, especialidade };
+    medicos.push(newMedico);
+
+    res.status(201).json(newMedico);
+});
+
+app.put("/medicos/atualizar/id/:id", (req: Request, res: Response) => {
+    const id = Number(req.params.id);
+    const  { crm, nome, especialidade} = req.body;
+
+    const userIndex = medicos.findIndex( u => u.id === id);
+
+    if (!crm || !nome || !especialidade) {
+        return res.status(400).json({ mensagem: "Todos os campos são obrigatórios para atualização" });
+    } else if(userIndex >= 0) {
+        medicos[userIndex] = { id, crm, nome, especialidade};
+        res.json(medicos[userIndex]);
+    } else {
+        res.status(404).json({ mensagem: "Médico não encontrado"});
+    }
+});
+
+app.put("/medicos/atualizar/crm/:crm", (req: Request, res: Response) => {
+    const crm = req.params.crm;
+    const { nome, especialidade } = req.body;
+
+    const userIndex = medicos.findIndex(u => u.crm === crm);
+
+    if (!nome || !especialidade) {
+        return res.status(400).json({ mensagem: "Nome e especialidade são obrigatórios para atualização" });
+    }
+
+    if (userIndex >= 0) {
+        const medicoAtual = medicos[userIndex];
+        medicos[userIndex] = { id: medicoAtual.id, crm: medicoAtual.crm, nome, especialidade };
+        return res.json(medicos[userIndex]);
+    } else {
+        return res.status(404).json({ mensagem: "Médico não encontrado" });
+    }
+});
+
+app.delete("/medicos/deletar/id/:id", (req, res) => {
+    const id = Number(req.params.id);
+    const userIndex = medicos.findIndex(u => u.id === id);
+
+    if(userIndex >= 0){
+        const removeUser = medicos.splice(userIndex, 1)[0];
+        res.json({ mensagem: "Médico removido", usuario: removeUser });
+    } else {
+        res.status(404).json({ mensagem: "Médico não encontrado"});
+    }
+});
+
+app.delete("/medicos/deletar/crm/:crm", (req, res) => {
+    const crm = String(req.params.crm);
+    const userIndex = medicos.findIndex(u => u.crm === crm);
+
+    if(userIndex >= 0){
+        const removeUser = medicos.splice(userIndex, 1)[0];
+        res.json({ mensagem: "Médico removido", usuario: removeUser });
+    } else {
+        res.status(404).json({ mensagem: "Médico não encontrado"});
+    }
+});
