df59e5ae99ecf492de24e7023fba3143f7b0f15e
LucasFerreira-Dev <lucasgabrieldasilvaferreira76@gmail.com>
2026-03-04 19:15:42 -0300

    Criando grafo m java

---
+++ Teoria_grafos/java/src/Vertex.java
@@ -0,0 +1,12 @@
+public class Vertex {
+    String name;
+
+    public Vertex(String name){
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
